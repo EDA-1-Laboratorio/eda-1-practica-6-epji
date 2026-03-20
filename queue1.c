00edf29936ea175885e525064a50695fd2fdd8ec
Padilla Huesca Edgar Ivan <edgar19ivan22@gmail.com>
2026-03-20 08:46:52 -0600

    Update queue1.c

--- queue1.c
+++ queue1.c
@@ -98,3 +98,5 @@ int main(void)
     }
     return 0;
 }
+
+//Pregunta: De acuerdo con lo que imprime la consola, ¿qué está haciendo el bloque main? El main separa en dos colas las cuales son a y b segun su tipo y despues imprime los elementos segun el orden en el que fueron ingresados. 
